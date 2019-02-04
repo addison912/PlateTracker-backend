3295cf01da78ed6b8967d65aee3b262006f1f116
Addison Moore <addisondrewmoore@gmail.com>
2019-02-04 02:41:58 -0800

    handle no image error

--- controllers/users.js
+++ controllers/users.js
@@ -17,46 +17,53 @@ module.exports = {
           console.log("hashing error:", err);
           res.status(200).json({ error: err });
         } else {
-          let date = Date.now();
           let fileName = "";
-          let base64Data;
-          if (/^data:image\/jpeg;base64,/.test(req.body.avatar)) {
-            base64Data = req.body.avatar.replace(
-              /^data:image\/jpeg;base64,/,
-              ""
+          let date = Date.now();
+          let path = null;
+          if (req.body.avatar) {
+            let base64Data;
+            if (/^data:image\/jpeg;base64,/.test(req.body.avatar)) {
+              base64Data = req.body.avatar.replace(
+                /^data:image\/jpeg;base64,/,
+                ""
+              );
+              fileName = `profilePic_${date}.jpeg`;
+            } else if (/^data:image\/png;base64,/.test(req.body.avatar)) {
+              base64Data = req.body.avatar.replace(
+                /^data:image\/png;base64,/,
+                ""
+              );
+              fileName = `profilePic_${date}.png`;
+            } else if (/^data:image\/gif;base64,/.test(req.body.avatar)) {
+              base64Data = req.body.avatar.replace(
+                /^data:image\/gif;base64,/,
+                ""
+              );
+              fileName = `profilePic_${date}.gif`;
+            } else console.log("invalid image type");
+            fs.writeFile(
+              `${__dirname}/../uploads/profilePics/${fileName}`,
+              base64Data,
+              "base64",
+              function(err) {
+                console.log(err);
+              }
             );
-            fileName = `profilePic_${date}.jpeg`;
-          } else if (/^data:image\/png;base64,/.test(req.body.avatar)) {
-            base64Data = req.body.avatar.replace(
-              /^data:image\/png;base64,/,
-              ""
-            );
-            fileName = `profilePic_${date}.png`;
-          } else if (/^data:image\/gif;base64,/.test(req.body.avatar)) {
-            base64Data = req.body.avatar.replace(
-              /^data:image\/gif;base64,/,
-              ""
-            );
-            fileName = `profilePic_${date}.gif`;
-          } else console.log("invalid image type");
-          fs.writeFile(
-            `${__dirname}/../uploads/profilePics/${fileName}`,
-            base64Data,
-            "base64",
-            function(err) {
-              console.log(err);
-            }
-          );
-          let path = `/uploads/profilePics/${fileName}`;
+            path = `/uploads/profilePics/${fileName}`;
+          }
 
           let newUser = {
             username: req.body.username,
             email: req.body.email,
             firstName: req.body.firstName,
             lastName: req.body.lastName,
-            avatar: path,
             password: hash
           };
+
+          if (path) {
+            newUser.avatar = path;
+          }
+
           User.findOne({ username: req.body.username }).then(user => {
             if (!user) {
               User.create(newUser).then(user => {
